f752c198eed08560a3c9e64cf9ddb0c9bf003af0
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-21 12:50:19 +0530

    Solution to Check Not Equal To

---
+++ Check Not Equal To@@67882a731b4392d8fc1577db/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+int first, second;
+
+scanf("%d %d", &first, &second);
+
+if (first != second)
+    printf("True\n");
+else 
+    printf("False\n");
+
+return 0;
+}
