50243749c420d69a8188e72c52d6a20829c82a8d
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-28 21:35:16 +0530

    Solution to Find Greater Number

---
+++ Find Greater Number@@6788dd63490637cfc44e1b8e/code.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int first, second;
+
+    scanf('%d %d', &first. &second);
+
+    if (first > second) {
+        printf('%d\n', first);
+    } else {
+        printf('%d\n', second);
+    }
+    return 0:
+}
