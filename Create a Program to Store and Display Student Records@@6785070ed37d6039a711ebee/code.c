317e30c3c11b9246b902de33be74be31e4d9e49d
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-30 22:36:22 +0530

    Solution to Create a Program to Store and Display Student Records

---
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct Student {
+    int roll_number;
+    char name[50];
+    int marks;
+}
+
+int main() {
+
+    int N;
+    int 
+    scanf("%d %d %c %d", &N);    
+
+
+
+    return 0;
+}
