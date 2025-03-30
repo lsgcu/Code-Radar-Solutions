a6360ba08c53ec3f27f5b4bdc7be5a098b9f020e
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-30 17:40:01 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int addition(int first, int second) {
+    return first += second;
+}
+
+int substraction(int first, int second) {
+    return first -= second;
+}
+
+int division(int first, int second) {
+    return first /= second;
+}
+
+int multiplication(int first, int second) {
+    return first *= second;
+}
+
+int main() {
+    int first, second input_operator;
+
+    scanf ("%d %d %d", &first, &second, &input_operator);
+
+    if (input_operator == '+') {
+        addition(first, second);
+        printf("%d\n", addition);
+    }
+
+return 0;
+}
