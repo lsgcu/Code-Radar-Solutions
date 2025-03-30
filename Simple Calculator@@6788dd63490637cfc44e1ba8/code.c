144920541e37b3ec83f2adf4baf785955f2e8bca
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-30 17:48:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -8,7 +8,9 @@ int substraction(int first, int second) {
     return first - second;
 }
 
-int division(int first, int second) {
+int division(int first, int second) {\
+    if (first == 0 || second == 0)
+        printf("Error\n");
     return first / second;
 }
 
@@ -23,20 +25,20 @@ int main() {
     scanf ("%d %d %c", &first, &second, &input_operator);
 
     if (input_operator == '+') {
-        int result = addition(first, second);
-        printf("%d\n", result);
+        int res1 = addition(first, second);
+        printf("%d\n", res1);
     }
     else if (input_operator == '/') {
-        int result = division(first, second);
-        printf("%d\n", result);
+        int res2 = division(first, second);
+        printf("%d\n", res2);
     }
     else if (input_operator == '*') {
-        int result = multiplication(first, second);
-        printf("%d\n", result);
+        int res3 = multiplication(first, second);
+        printf("%d\n", res3);
     }
     else if (input_operator == '-') {
-        int result = substraction(first, second);
-        printf("%d\n", result);
+        int res4 = substraction(first, second);
+        printf("%d\n", res4);
     }
 
 return 0;
