9fb79c540afcc6b15f021ab000060afdec51bed5
lsgcu <leopold1001.cse24@chitkara.edu.in>
2025-03-31 15:49:20 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -6,21 +6,22 @@ int N;
 
 scanf("%d", &N);
 
-    // partie supp diamand
-    for (int i = 1; i <= N; i++) {
-        printf("i=%d", i);
-        printf("N=%d", N);
-        //espaces
-        for (int j = 1; j <= (N - i); j++) {
-            printf("j=%d", j);
+    for (int i = 1; i <= N; i++) { // partie supp
+        for (int j = 1; j <= (N - i); j++) { // espaces
             printf(" ");
         }
-        //etoiles
-        for (int k = 1; k <= (2*i- 1); k++) {
-            printf("k=%d", k);
+        for (int k = 1; k <= (2*i- 1); k++) { // etoiles
             printf("*");
         }
         printf("\n");
+    }
+    for (int l = 1; l <= N - 1; l++) { // partie inf
+        for (int m = 1; l <= (N + 1); m++) {
+            printf(" ");
+        }
+        for (int o = 1; o <= (2*i + 1); o++) {
+            printf("*");
+        }
     }
     return 0;
 }
